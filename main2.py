146b64e595a87531f3dff76960195727691afed9
Novozhilov <dndiamond718@gmail.com>
2022-12-08 19:19:45 +0300

    Multiprocessing coding refuctoring. Work but in parallel processing result of count incorrect.

--- main2.py
+++ main2.py
@@ -5,12 +5,11 @@ import multiprocessing
 from multiprocessing import Process, Queue, Value, Manager
 from datetime import datetime
 
-# vod = 0;live = 0;cuts = 0
-
 start_time=datetime.now()
 
 #file = sys.argv[1:]
-file = {"test.log","test.log"}
+file = {"test.log","test2.log"}
+#file = {"hcs.log","hcs1.log"}
 
 # def test(j,vod,live):
 #     """Function for count request percentage of VOD/Live/CU and HIT/MISS percentage of this types"""
@@ -26,46 +25,44 @@ file = {"test.log","test.log"}
 
 def argument(m,a,n):
      proc_num = os.getpid()
-     print(m, proc_num,end=' \n')
      with open(os.getcwd()+'\/'+m, newline='') as hcs_1:
          hcs_2 = csv.reader(hcs_1, delimiter=' ')
          # for hcs in hcs_2:
          for j in hcs_2:
              if j[3].find('MISS') != -1:
-                 shared = a["vod"][n]
-                 shared.append(1)
-                 a["vod"][n] = shared
-                 print(a)
-             # elif j[3].find('HIT') != -1:
-             #     a["vod"][n] += 10
-#     print (a)
+                 a_temp_m = a["vod_miss"]
+                 a_temp_m[n] += 1
+                 a["vod_miss"] = a_temp_m
+                 #print(a)
+             elif j[3].find('HIT') != -1:
+                 a_temp_h = a["vod_hit"]
+                 a_temp_h[n] += 1
+                 a["vod_hit"] = a_temp_h
+                 #print(a)
+     print(f'Log file = {n}',m, 'Processor = ',proc_num, end=' \n')
+     print (a)
      end_time = datetime.now()
      print('      Duration: {}'.format(end_time - start_time))
-
-
+     print()
 
 if __name__ == '__main__':
     procs = []
     manager = Manager()
     vod_live_cuts = manager.dict()
-    i = "vod"
-    cpu = 1
+    i = "vod_hit"
+    ii = "vod_miss"
+    cpu = 2
     n = 0
     vod_live_cuts[i] = [0] * cpu
-    #vod_live_cuts[i] = manager.list()
+    vod_live_cuts[ii] = [0] * cpu
     for m in file:
         proc = Process(target=argument, args=(m,vod_live_cuts,n))
         n += 1
         procs.append(proc)
         proc.start()
-        proc.join()
-
+        #proc.join()
+    [proc.join() for proc in procs]
     end_time = datetime.now()
-    print('      Duration: {}'.format(end_time - start_time))
-    # print(vod.value,live.value)
+    print('Total      Duration: {}'.format(end_time - start_time))
     print(vod_live_cuts)
-    # print()
-    # print('1.   % between VOD/LIVE/CU  (all requests)')
-    # print()
-    # print('%14s %5.1f %-3s %8s %5.1f %0s %8s %5.1f %0s' % ('VOD =',(vod/(live+vod+cuts)*100),'%','Live =',(live/(live+vod+cuts)*100),'%','CU =',(cuts/(live+vod+cuts)*100),'%'))
-    # print()
+    [proc.close() for proc in procs]
