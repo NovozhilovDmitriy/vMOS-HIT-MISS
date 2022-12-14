b45fcd642b2f021291c87215e1b4744b2a53c9d2
Novozhilov <dndiamond718@gmail.com>
2022-12-14 10:20:14 +0300

    refuctoring multiprocessing - Worked. WOW.

--- main_multiprocess.py
+++ main_multiprocess.py
@@ -1,62 +1,34 @@
 import csv
-import sys
 import os
-import multiprocessing
-from multiprocessing import Process, Queue, Value, Manager
+from multiprocessing import Process, Queue, Value, Manager, Pool
 from datetime import datetime
 
 start_time=datetime.now()
 
 #file = sys.argv[1:]
 #file = {"test3.log", "test4.log"}
-#file = {"hcs.log", "hcs1.log", "hcs2.log", "hcs3.log"}
-file = {"hcs.log"}
+file = {"hcs.log", "hcs1.log", "hcs2.log", "hcs3.log"}
+#file = {"hcs.log"}
 
-# def test(j,vod,live):
-#     """Function for count request percentage of VOD/Live/CU and HIT/MISS percentage of this types"""
-#     for j in j:
-#          if j.find('MISS') != -1:
-#              vod.value+=1
-#          elif j.find('HIT') != -1:
-#              live.value += 1
-#          # elif j.find('servicetype=3') != -1:
-#          #      cuts += 1
-#          # return(vod,live,cuts)
-#          # print(j)
-
-
-def argument(m, a, n):
+def argument(m, a):
      proc_num = os.getpid()
-     a_temp_m = a["vod_miss"]
-     a_temp_h = a["vod_hit"]
-     print(f'Processor = {proc_num} , Log file = {n} , {m}  , INPUT_1  : a[vod_hit] = ', a["vod_hit"], ', a[vod_miss] = ', a["vod_miss"], ' a_temp_h  = ', a_temp_h, ', a_temp_m  = ', a_temp_m)
+     a_temp_m = 0
+     a_temp_h = 0
+     print(f'Processor = {proc_num} , Log file = {m}  , INPUT_1  : a[vod_hit] = ', a["vod_hit"], ', a[vod_miss] = ', a["vod_miss"], ' a_temp_h  = ', a_temp_h, ', a_temp_m  = ', a_temp_m)
      with open(os.getcwd() + '\/' + m, newline='') as hcs_1:
          hcs_2 = csv.reader(hcs_1, delimiter=' ')
-         # for hcs in hcs_2:
          for j in hcs_2:
              if j[3].find('MISS') != -1:
-                 #a_temp_m = a["vod_miss"]
-                 a_temp_m[n] = a_temp_m[n] + 1
-                 #a["vod_miss"][n] += 1
-                 #a["vod_miss"] = a_temp_m
-                 end_time = datetime.now()
-                 #print(f'Log file = {n} , ', m, ', Processor = ', proc_num, ', update - ', j[3][-4:], ' - ', a_temp_m,
-                 #      '      Duration: {}'.format(end_time - start_time))
+                 a_temp_m = a_temp_m + 1
              elif j[3].find('HIT') != -1:
-                 #a_temp_h = a["vod_hit"]
-                 a_temp_h[n] = a_temp_h[n] + 1
-                 #a["vod_hit"][n] += 1
-                 #a["vod_hit"] = a_temp_h
-                 end_time = datetime.now()
-                 #print(f'Log file = {n} , ', m, ', Processor = ', proc_num, ', update - ', j[3][-4:], ' - ', a_temp_h,
-                 #      '      Duration: {}'.format(end_time - start_time))
-     print(f'Log file = {n} , ', m, f'a[vod_hit][{n}] = ', a["vod_hit"][n],f', a_temp_h[{n}]=', a_temp_h[n], f', a[vod_miss][{n}] =', a["vod_miss"][n], f', a_temp_m[{n}]=', a_temp_m[n])
-     print(f'Log file = {n} , ', m, 'INPUT_2  : a[vod_hit] = ', a["vod_hit"], ', a[vod_miss] = ', a["vod_miss"])
-     print(f'Log file = {n} , ', m, 'OUTPUT : a_temp_h  = ', a_temp_h, ', a_temp_m  = ', a_temp_m)
-     a["vod_miss"][n] = a_temp_m[n]
-     a["vod_hit"][n] = a_temp_h[n]
+                 a_temp_h = a_temp_h + 1
+     print(f'Log file = {m}', f'a[vod_hit] = ', a["vod_hit"],f', a_temp_h=', a_temp_h, f', a[vod_miss] =', a["vod_miss"], f', a_temp_m=', a_temp_m)
+     print(f'Log file = {m}', 'INPUT_2  : a[vod_hit] = ', a["vod_hit"], ', a[vod_miss] = ', a["vod_miss"])
+     print(f'Log file = {m}', 'OUTPUT : a_temp_h  = ', a_temp_h, ', a_temp_m  = ', a_temp_m)
+     a["vod_miss"].append(a_temp_m)
+     a["vod_hit"].append(a_temp_h)
      end_time = datetime.now()
-     #print(f'Log file = {n} , ', m, ', Processor = ', proc_num, ', vod_live_cuts =', a, '      Duration: {}'.format(end_time - start_time))
+     print(f'Log file = {m} ', ' Processor = ', proc_num, ', vod_live_cuts =', a["vod_hit"], a["vod_miss"], '      Duration: {}'.format(end_time - start_time))
      print()
 
 
@@ -67,24 +39,14 @@ if __name__ == '__main__':
     i = "vod_hit"
     ii = "vod_miss"
     cpu = 1
-    n = 1
-    # vod_live_cuts[i] = [0] * cpu
-    # vod_live_cuts[ii] = [0] * cpu
-    vod_live_cuts[i] = manager.list([0] * cpu)
-    vod_live_cuts[ii] = manager.list([0] * cpu)
-    for m in file:
-        proc = Process(target=argument, args=(m, vod_live_cuts, (n-1)))
-        procs.append(proc)
-        proc.start()
-        if n >= cpu:
-            n = 1
-            proc.join()
-        else:
-            n += 1
-        #proc.join()
-    [proc.join() for proc in procs]
+    vod_live_cuts[i] = manager.list()
+    vod_live_cuts[ii] = manager.list()
+    with Pool(cpu) as pool:
+        tasks = []
+        for m in file:
+            task = pool.apply_async(argument, args=(m, vod_live_cuts))
+            tasks.append(task)
+        for task in tasks:
+            task.get()
     end_time = datetime.now()
     print('                                                update - ', vod_live_cuts["vod_hit"], vod_live_cuts["vod_miss"], '       Duration: {}'.format(end_time - start_time))
-    #print('Total      Duration: {}'.format(end_time - start_time))
-    #print(vod_live_cuts)
-    [proc.close() for proc in procs]
